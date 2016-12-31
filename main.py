64b38b17f7d05444ad680856c6b658fb22755dc2
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2016-12-31 02:09:17 +0200

    Boilerplate code for visualqa.

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# IMPORTANT: Do not rely on constants as experiment parameters. Keep track of
+# all experiment params in a central dictionary, for easy pickling (this
+# aids reproducibility a lot).
+
+import logging
+
+from visualqa.bow_vgg_baseline import BoWVGGBaseline
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    baseline = BoWVGGBaseline()
+    baseline.train()
+
+
+if __name__ == '__main__':
+    main()
---
+++ visualqa/bow_vgg_baseline.py
@@ -0,0 +1,13 @@
+"""Simple baseline using word vector adding an VGG image embeddings."""
+
+import logging
+
+log = logging.getLogger('bow_vgg_baseline')
+
+class BoWVGGBaseline(object):
+
+    def __init__(self, **kw):
+        pass
+
+    def train(self):
+        log.info("Training BoW VGG Baseline...")
